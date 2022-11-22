a58059a0aad592dd5aa3cd5eb7504a83d5aaabc1
Algernon Soriano <alger.soriano@gmail.com>
2022-11-22 19:35:10 +1000

    countdown

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown(a)
+    x = a
+    while x > 0
+        puts "#{x} SECOND(S)!"
+        x -= 1
+    end
+    return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(b)
+    x = 1
+    while x > 0
+        puts "#{x} SECOND(S)!"
+        x -= 1
+        sleep(b)
+    end
+    return "HAPPY NEW YEAR!"
+    
+end
+
+countdown_with_sleep(1)
